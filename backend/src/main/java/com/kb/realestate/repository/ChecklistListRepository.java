13ee3129b7b490411bd2924976b0c975f89bb3c3
songeseudeo <qetuo4751@naver.com>
2026-06-12 20:54:00 +0900

    fix: DB 의존성 제거, 서버 DB 없이 동작

--- backend/src/main/java/com/kb/realestate/repository/ChecklistListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kb.realestate.repository;
-
-import com.kb.realestate.entity.ChecklistList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChecklistListRepository extends JpaRepository<ChecklistList, Long> {
-}
